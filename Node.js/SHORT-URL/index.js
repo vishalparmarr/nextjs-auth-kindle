ad53d2ad084f17481267661efb00bc9c9bc5b4fb
vishalparmarr <vishalbusiness3108@gmail.com>
2024-05-27 18:38:36 +0530

    nextjsauth

--- Node.js/SHORT-URL/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const app = express();
-const PORT = 8001;
-
-app.listen(PORT, () => {
-  console.log(`Server is started at port ${PORT}`);
-});
--- nextjs-auth-kinde/app/api/auth/[kindeAuth]/route.tsx
+++
@@ -1,2 +0,0 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
